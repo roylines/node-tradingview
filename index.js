4bfb5ef0b714ddedb3a6374a64a51c3b2dee30da
Roy Lines <roy@roylines.co.uk>
2021-03-03 07:13:46 +0000

    added calls to scanner

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const get = async() => {
-}
+const axios = require("axios");
+
+const get = async (options) => {
+  const {
+    ticker = "COINBASE:BTCGBP",
+    resolution = 1,
+    scanner = "crypto",
+    indicator = "Recommend.All",
+  } = options;
+
+  const body = {
+    symbols: { tickers: [ticker], query: { types: [] } },
+    columns: [`${indicator}|${resolution}`],
+  };
+  const {
+    data: { data },
+  } = await axios.post(`https://scanner.tradingview.com/${scanner}/scan`, body);
+
+  const signal = data[0].d[0];
+  return { ...options, signal };
+};
 
 module.exports = {
-  signals: {
-    get
-  }
-}
+  get,
+};
